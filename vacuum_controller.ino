af648410a2acded1044b371d95e5035bea46593b
gearedrock <gearedrock@yahoo.com>
2020-03-28 12:04:49 -0700

    Basic motor control.

--- vacuum_controller.ino
+++ vacuum_controller.ino
@@ -21,6 +21,12 @@ int adc_key_in  = 0;
 #define btnSELECT 4
 #define btnNONE   5
 
+#define enB 11
+#define in4 12
+#define in3 13
+int speed = 0;
+int bounce = 0;
+
 // read the buttons
 int read_LCD_buttons()
 {
@@ -28,13 +34,7 @@ int read_LCD_buttons()
  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
  // we add approx 50 to those values and check to see if we are close
  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
- // For V1.1 us this threshold
-/* if (adc_key_in < 50)   return btnRIGHT;
- if (adc_key_in < 250)  return btnUP;
- if (adc_key_in < 450)  return btnDOWN;
- if (adc_key_in < 650)  return btnLEFT;
- if (adc_key_in < 850)  return btnSELECT;
-*/
+ 
  // For V1.0 comment the other threshold and use the one below:
 
  if (adc_key_in < 50)   return btnRIGHT;
@@ -53,16 +53,28 @@ void setup()
  lcd.begin(16, 2);              // start the library
  lcd.setCursor(0,0);
  lcd.print("Push the buttons"); // print a simple message
+
+ pinMode(enB, OUTPUT);
+ pinMode(in3, OUTPUT);
+ pinMode(in4, OUTPUT);
+ digitalWrite(in3, LOW);
+ digitalWrite(in4, HIGH);
 }
 
 void loop()
 {
  lcd.setCursor(9,1);            // move cursor to second line "1" and 9 spaces over
- lcd.print(millis()/1000);      // display seconds elapsed since power-up
-
+ lcd.print(speed, 16);      // display seconds elapsed since power-up
+ lcd.print("-b");
+ lcd.print(bounce, 16);
 
  lcd.setCursor(0,1);            // move to the begining of the second line
  lcd_key = read_LCD_buttons();  // read the buttons
+ if(lcd_key!=btnNONE){
+  bounce+=1;
+ }else{
+  bounce = 0;
+ }
 
  switch (lcd_key)               // depending on which button was pushed, we perform an action
  {
@@ -79,11 +91,28 @@ void loop()
    case btnUP:
      {
      lcd.print("UP    ");
+     if(bounce%100==0){
+      speed += 10;
+     }else if(bounce%10==0){
+      speed += 1;
+     }
+     //speed+=10;
+     if(speed>255){
+      speed = 255;
+     }
      break;
      }
    case btnDOWN:
      {
      lcd.print("DOWN  ");
+     if(bounce%100==0){
+      speed -= 10;
+     }else if(bounce%10==0){
+      speed -= 1;
+     }
+     if(speed<0){
+      speed = 0;
+     }
      break;
      }
    case btnSELECT:
@@ -97,5 +126,6 @@ void loop()
      break;
      }
  }
+ analogWrite(enB, speed);
 
 }
